dcbc464df8c06f006a4b9b60c93644ab09cb4386
hanadaUG <hanadaUG@gmail.com>
2020-02-28 01:11:20 +0900

    暗号化の仮実装

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -9,7 +11,9 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
+	"io"
 	"log"
+	"strings"
 	"time"
 
 	"os"
@@ -54,9 +58,40 @@ $ aws-s3-json-uploader <id> <password>`
 
 	// jsonデータを生成します
 	t := time.Now()
-	const layout = "20060102"
+	const layout = "2006/01/02"
 	data, _ := json.MarshalIndent(AuthInfo{Id: args[0], Password: args[1],LastUpdated: t.Format(layout)}, "", "\t")
 	fmt.Println(string(data))
+
+	key := []byte("1234567890xxxxxxxxxx1234567890ab")
+	// 32byte = AES-256
+	fmt.Printf("key: %s(%d byte)\n", key, len(key))
+
+	// Create new AES cipher block
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Printf("err: %s\n", err)
+	}
+
+	// Create IV
+	cipherText := make([]byte, aes.BlockSize+len(data))
+	iv := cipherText[:aes.BlockSize]
+	//if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+	if _, err := io.ReadFull(strings.NewReader("1234567890abcdef"), iv); err != nil {
+
+		fmt.Printf("err: %s\n", err)
+	}
+	fmt.Printf("iv: %x \n", iv)
+
+	// Encrypt
+	encryptStream := cipher.NewCTR(block, iv)
+	encryptStream.XORKeyStream(cipherText[aes.BlockSize:], data)
+	fmt.Printf("Cipher text: %x \n", cipherText)
+
+	// Decrypt
+	decryptedText := make([]byte, len(cipherText[aes.BlockSize:]))
+	decryptStream := cipher.NewCTR(block, cipherText[:aes.BlockSize])
+	decryptStream.XORKeyStream(decryptedText, cipherText[aes.BlockSize:])
+	fmt.Printf("Decrypted text: %s\n", string(decryptedText))
 
 	// AWS S3にアクセスするためひ必要なアクセスキーとシークレットキーをcsvから読み込みます
 	// IAMでユーザーを新規作成時にダウンロードできる「credentials.csv」を読み込みます
@@ -82,9 +117,9 @@ $ aws-s3-json-uploader <id> <password>`
 	// jsonをS3にアップロードします
 	_, err = client.PutObject(&s3.PutObjectInput{
 		Bucket:      aws.String("xxx"), // TODO:
-		Key:         aws.String("./test.json"),
+		Key:         aws.String("./test"),
 		ContentType: aws.String("application/json"),
-		Body:        bytes.NewReader(data),
+		Body:        bytes.NewReader(cipherText),
 	})
 	if err != nil {
 		log.Fatal(err)
