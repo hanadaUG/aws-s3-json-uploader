f95bd1f7f03db9c82c06fd965f5e7414d0c77630
hanadaUG <hanadaUG@gmail.com>
2020-03-01 03:00:34 +0900

    暗号化と符号化を追加

--- main.go
+++ main.go
@@ -4,27 +4,25 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/base64"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"time"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"io"
-	"log"
-	"strings"
-	"time"
-
-	"os"
-
 	"github.com/gocarina/gocsv"
 )
 
 // authentication information
 type AuthInfo struct {
-	Id       string `json:"username"`
-	Password string `json:"password"`
+	UserName    string `json:"username"`
+	Password    string `json:"password"`
 	LastUpdated string `json:"updatedAt"`
 }
 
@@ -59,39 +57,48 @@ $ aws-s3-json-uploader <id> <password>`
 	// jsonデータを生成します
 	t := time.Now()
 	const layout = "2006/01/02"
-	data, _ := json.MarshalIndent(AuthInfo{Id: args[0], Password: args[1],LastUpdated: t.Format(layout)}, "", "\t")
-	fmt.Println(string(data))
+	data, err := json.MarshalIndent(AuthInfo{UserName: args[0], Password: args[1], LastUpdated: t.Format(layout)}, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("json  : %s (%d byte)\n", string(data), len(data))
+	fmt.Printf("json  : %x (%d byte)\n", data, len(data))
 
-	key := []byte("1234567890xxxxxxxxxx1234567890ab")
-	// 32byte = AES-256
-	fmt.Printf("key: %s(%d byte)\n", key, len(key))
+	// 暗号化
+	key := []byte("00000000000000000000000000000000") // 32 bytes => AES-256
+	fmt.Printf("key   : %s (%d bytes)\n", key, len(key))
 
-	// Create new AES cipher block
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		fmt.Printf("err: %s\n", err)
+		panic(err)
 	}
 
-	// Create IV
-	cipherText := make([]byte, aes.BlockSize+len(data))
-	iv := cipherText[:aes.BlockSize]
-	//if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-	if _, err := io.ReadFull(strings.NewReader("1234567890abcdef"), iv); err != nil {
+	iv := []byte("0000000000000000")
+	fmt.Printf("iv    : %s (%d bytes)\n", iv, len(iv))
 
-		fmt.Printf("err: %s\n", err)
-	}
-	fmt.Printf("iv: %x \n", iv)
+	cbc := cipher.NewCBCEncrypter(block, iv)
+	padding := PKCS5Padding(data)
+	fmt.Printf("PKCS#5: %x (%d bytes)\n", padding, len(padding))
 
-	// Encrypt
-	encryptStream := cipher.NewCTR(block, iv)
-	encryptStream.XORKeyStream(cipherText[aes.BlockSize:], data)
-	fmt.Printf("Cipher text: %x \n", cipherText)
+	encrypt := make([]byte, len(padding))
+	cbc.CryptBlocks(encrypt, padding)
+	fmt.Printf("暗号化: %x (%d bytes)\n", encrypt, len(encrypt))
 
-	// Decrypt
-	decryptedText := make([]byte, len(cipherText[aes.BlockSize:]))
-	decryptStream := cipher.NewCTR(block, cipherText[:aes.BlockSize])
-	decryptStream.XORKeyStream(decryptedText, cipherText[aes.BlockSize:])
-	fmt.Printf("Decrypted text: %s\n", string(decryptedText))
+	enc := base64.StdEncoding.EncodeToString(encrypt)
+	fmt.Printf("符号化: %s (%d byte)\n", enc, len(enc))
+
+	//dec, err := base64.StdEncoding.DecodeString(enc)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Printf("decode: %x\n", dec)
+	//plainText := make([]byte, len(dec))
+	//cbc = cipher.NewCBCDecrypter(block, iv)
+	//cbc.CryptBlocks(plainText, dec)
+	//fmt.Printf("復号化: %s (%d bytes)\n", PKCS5Trimming(plainText), len(PKCS5Trimming(plainText)))
+
+	// ローカルに暗号化したバイナリーデータを保存します
+	//ioutil.WriteFile("encrypt", []byte(enc), 0644)
 
 	// AWS S3にアクセスするためひ必要なアクセスキーとシークレットキーをcsvから読み込みます
 	// IAMでユーザーを新規作成時にダウンロードできる「credentials.csv」を読み込みます
@@ -116,12 +123,30 @@ $ aws-s3-json-uploader <id> <password>`
 
 	// jsonをS3にアップロードします
 	_, err = client.PutObject(&s3.PutObjectInput{
-		Bucket:      aws.String("xxx"), // TODO:
-		Key:         aws.String("./test"),
-		ContentType: aws.String("application/json"),
-		Body:        bytes.NewReader(cipherText),
+		Bucket: aws.String("xxx"), // TODO:
+		Key:    aws.String("./sample"),
+		Body:   bytes.NewReader([]byte(enc)),
+		ACL:    aws.String(s3.BucketCannedACLPublicRead),
 	})
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// PKCS#5 Padding
+// ブロック長に満たないサイズ（＝埋めるサイズ）の値を表すバイト値で足りない分を埋めます
+// (例) ブロック長が16バイトでデータサイズが72バイトなら0x08で足りない8バイト分埋めます
+// ブロック長ぴったりな場合は1ブロック分丸ごとパディングされます
+func PKCS5Padding(data []byte) []byte {
+	//fmt.Printf("data: %d bytes\n", len(data))
+	padding := aes.BlockSize - len(data)%aes.BlockSize
+	//fmt.Printf("padding: %xbytes\n", padding)
+	pad := bytes.Repeat([]byte{byte(padding)}, padding)
+	//fmt.Printf("pad: %x\n", pad)
+	return append(data, pad...)
+}
+
+func PKCS5Trimming(encrypt []byte) []byte {
+	padding := encrypt[len(encrypt)-1]
+	return encrypt[:len(encrypt)-int(padding)]
 }
